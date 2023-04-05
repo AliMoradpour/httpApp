36d70c2514a85e7f495ad6d5eebae18371fc1a4c
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-05 14:55:56 +0430

    make project structure

---
+++ src/Components/FullComment/FullComment.jsx
@@ -0,0 +1,13 @@
+import "./fullcomment.css";
+
+const FullComment = () => {
+  return (
+    <section className="fullComment">
+      <p>name</p>
+      <p>email</p>
+      <p>body</p>
+    </section>
+  );
+};
+
+export default FullComment;
---
+++ src/Components/NewComment/NewComment.jsx
@@ -0,0 +1,23 @@
+import "./newcomment.css";
+
+const NewComment = () => {
+  return (
+    <div className="newComment">
+      <div>
+        <label>name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>email</label>
+        <input type="email" />
+      </div>
+      <div>
+        <label>body</label>
+        <input type="text" />
+      </div>
+      <button>Add</button>
+    </div>
+  );
+};
+
+export default NewComment;
