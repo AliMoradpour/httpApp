0178e41d15a6ee8d3eadc73458c36753876a859b
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-09 12:56:13 +0430

    add header to post Req and Complete the project

--- src/Services/Requests.js
+++ src/Services/Requests.js
@@ -3,12 +3,18 @@ import http from "./httpServices";
 export function getAllComments() {
   return http.get("/comments");
 }
-export function getOneComment(id){
-  return http.get(`/comments/${id}`)
+export function getOneComment(id) {
+  return http.get(`/comments/${id}`);
 }
 export function deleteComment(id) {
   return http.delete(`/comments/${id}`);
 }
 export function postComment(data) {
-  return http.post("/comments", data);
+  const token = "SECURE TOKEN";
+  const header = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return http.post("/comments", data, header);
 }
